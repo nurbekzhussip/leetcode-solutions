a58435bce316a6221dae44456f3da05a4820a477
Nurbek Zhussip <NAZhusip@sber-solutions.kz>
2021-10-31 17:22:02 +0600

    feat: add #206

---
+++ everse-linked-list/206.js
@@ -0,0 +1,16 @@
+//#206
+//https://leetcode.com/problems/reverse-linked-list/
+
+var reverseList = function (head) {
+  let prev = null;
+  let current = head;
+
+  while (current) {
+    let next = current.next;
+    current.next = prev;
+    prev = current;
+    current = next;
+  }
+
+  return prev;
+};
