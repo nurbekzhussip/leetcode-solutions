20bebf6ff9cc7c3c15a62facdd5aaf0d5661ac89
Nurbek Zhussip <NAZhusip@sber-solutions.kz>
2021-10-31 17:24:25 +0600

    feat: add #704

---
+++ binary-search/704.js
@@ -0,0 +1,20 @@
+//#704
+//https://leetcode.com/problems/binary-search/
+
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.round(right - (right - left) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+};
