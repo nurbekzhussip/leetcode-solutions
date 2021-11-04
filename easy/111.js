475f3177922b2c93bc869207b332f83a98f4b1e8
Nurbek Zhussip <NAZhusip@sber-solutions.kz>
2021-11-04 09:05:21 +0600

    refactor: add solution to #111

--- easy/111.js
+++ easy/111.js
@@ -16,3 +16,37 @@ var minDepth = function (root) {
 
   return 1 + Math.min(minLeft, minRight);
 };
+
+//Solution 2
+var minDepth = function (root) {
+  if (!root) {
+    return 0;
+  }
+
+  let depth = 1;
+  let queue = [root];
+
+  while (queue.length) {
+    let n = queue.length;
+
+    for (let i = 0; i < n; i++) {
+      let node = queue.shift();
+
+      if (!node.left && !node.right) {
+        return depth;
+      }
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    depth++;
+  }
+
+  return depth;
+};
