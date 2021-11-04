07d5b93144e3f395f517a65eabdedce649283e44
Nurbek Zhussip <NAZhusip@sber-solutions.kz>
2021-11-04 08:09:51 +0600

    refactor: update #111

--- easy/111.js
+++ easy/111.js
@@ -16,31 +16,3 @@ var minDepth = function (root) {
 
   return 1 + Math.min(minLeft, minRight);
 };
-
-//Solution 2
-var averageOfLevels = function (root) {
-  let queue = [root];
-  let res = [];
-
-  while (queue.length) {
-    //let node = queue.pop()
-    let n = queue.length;
-    let level_sum = 0;
-
-    for (let i = 0; i < n; i++) {
-      let node = queue.shift();
-      level_sum += node.val;
-
-      if (node.left) {
-        queue.push(node.left);
-      }
-      if (node.right) {
-        queue.push(node.right);
-      }
-    }
-
-    res.push(level_sum / n);
-  }
-
-  return res;
-};
