6d5c602c5fa3c0e8cf39576350951fc3700d1862
Nurbek Zhussip <NAZhusip@sber-solutions.kz>
2021-11-13 21:29:59 +0600

    refactor: update code

--- easy/100.js
+++ easy/100.js
@@ -1,6 +1,7 @@
 //#100
 //https://leetcode.com/problems/same-tree/
 
+//Solution 1
 if (!p && !q) {
   return true;
 } else if (!p || !q) {
@@ -12,3 +13,19 @@ if (p.val === q.val) {
 } else {
   return false;
 }
+
+
+//Solution 2
+var isSameTree = function(p, q) {
+    
+  if(!p && !q){
+      return true
+  }else if(!p || !q){
+      return false
+  }
+  
+  if(p.val === q.val){
+      return isSameTree(p.right, q.right) && isSameTree(p.left, q.left)
+  }else{
+      return false
+  }
--- easy/226.js
+++ easy/226.js
@@ -15,3 +15,85 @@ var invertTree = function (root) {
 
   return root;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// common
+
+1. Questionnaire controller architecture
+
+Create a Questionnaire UI controller.
+validation should be on each step: 
+- ‘text’ answer type - optional answer
+- ‘multiChoice’ - 1 answer required
+- ‘singleChoice’ - 1 answer required
+send answers to BE after all steps are completed
+after reload questionnaire starts from scratch
+
+
+
+enum QuestionCategory {
+covid = 1,
+other = 2
+}
+
+type QuestionId = number;
+
+interface QuestionAnswer {
+id: string; 
+text: string; 
+nextQuestionId?: QuestionId;
+}
+
+type QuestionAnswerType = ‘text’ | ‘multiChoice’ | ‘singleChoice’;
+
+interface Question {
+	isFirst?: boolean;
+id: QuestionId;
+	text: string;
+answearType: QuestionAnswerType;
+category: QuestionCategory;
+answers?: Array<QuestionAnswer>;
+nextQuestionId?: QuestionId;
+}
+
+GET questions from BE: Array<Question>;
+
+type AnswerValue = string; // id, free text 
+
+POST:
+Array<{ [questionId: string]: AnswerValue | Array<AnswerValue> }>
+
+2. Dynamic layout architecture
+https://www.ixbt.com/img/n1/news/2020/0/1/Facebook_tmavy%CC%81_rez%CC%8Cim_large.jpg
+
+Changing layout structure dynamically (show, hide header, sidebar, change header buttons, background color) if plenty of pages (50+)
+
