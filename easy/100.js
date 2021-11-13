a7740b395660bcf3ebd17e4e577274ace0ba115a
Nurbek Zhussip <NAZhusip@sber-solutions.kz>
2021-11-13 21:48:04 +0600

    feat: add #112

--- easy/100.js
+++ easy/100.js
@@ -14,18 +14,43 @@ if (p.val === q.val) {
   return false;
 }
 
-
 //Solution 2
-var isSameTree = function(p, q) {
-    
-  if(!p && !q){
-      return true
-  }else if(!p || !q){
-      return false
-  }
-  
-  if(p.val === q.val){
-      return isSameTree(p.right, q.right) && isSameTree(p.left, q.left)
-  }else{
-      return false
-  }
+// var isSameTree = function(p, q) {
+
+//   if(!p && !q){
+//       return true
+//   }else if(!p || !q){
+//       return false
+//   }
+
+//   if(p.val === q.val){
+//       return isSameTree(p.right, q.right) && isSameTree(p.left, q.left)
+//   }else{
+//       return false
+//   }
+// }
+
+//Solution 3
+// var isSameTree = function (p, q) {
+//   let queue = [p, q];
+
+//   while (queue.length) {
+//     let p1 = queue.shift();
+//     let q1 = queue.shift();
+
+//     if (!p1 && !q1) {
+//       continue;
+//     }else if (!p1 || !q1) {
+//       return false;
+//     }
+//     if (p1.val !== q1.val) {
+//       return false;
+//     }
+
+//     queue.push(p1.left)
+//     queue.push(q1.left)
+//     queue.push(p1.right)
+//     queue.push(q1.right)
+//   }
+//     return true
+// };
---
+++ easy/112.js
@@ -0,0 +1,43 @@
+//112
+//https://leetcode.com/problems/path-sum/
+
+//Solution 1
+var hasPathSum = function (root, targetSum) {
+  let hasTotal = false;
+  const dfs = (node, target, total = 0) => {
+    if (!node) {
+      return null;
+    }
+
+    if (node.left) {
+      dfs(node.left, target, total + node.val);
+    }
+
+    if (node.right) {
+      dfs(node.right, target, total + node.val);
+    }
+    if (!node.left && !node.right) {
+      if (total + node.val === target) {
+        hasTotal = true;
+      }
+    }
+  };
+  dfs(root, targetSum);
+  return hasTotal;
+};
+
+//Solution 2
+var hasPathSum = function (root, targetSum) {
+  if (!root) {
+    return false;
+  }
+  targetSum -= root.val;
+
+  if (!root.right && !root.left) {
+    return targetSum === 0;
+  } else {
+    return (
+      hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum)
+    );
+  }
+};
