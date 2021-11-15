595c015593c769a41ef21aa7080fd5f97ac4be7f
Nurbek Zhussip <NAZhusip@sber-solutions.kz>
2021-11-15 23:20:04 +0600

    refactor: update

---
+++ extra/1.js
@@ -0,0 +1,9 @@
+// function apply(repeat, fn, value) {
+//   let counter = 0;
+
+//   while (counter < repeat) {
+//     value = fn(value);
+//     console.log(value);
+//     counter++;
+//   }
+// }
